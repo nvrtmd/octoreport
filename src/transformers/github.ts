6cd3834056bf3ab822ee5955b5b99159b21f64f6
yuza <untmd.crmn@gmail.com>
2025-07-02 16:09:14 +0900

    feat: add pr data transformers

---
+++ src/transformers/github.ts
@@ -0,0 +1,34 @@
+import { PR } from "../types";
+import { PRListItem, PRDetail } from "../schemas/github";
+
+function transformPRListItem(
+  listItem: PRListItem
+): Omit<PR, "labels" | "author" | "reviewers"> {
+  return {
+    number: listItem.number,
+    title: listItem.title,
+    url: listItem.html_url,
+    createdAt: listItem.created_at,
+    mergedAt: listItem.pull_request.merged_at ?? undefined,
+  };
+}
+
+function transformPRDetail(
+  detail: PRDetail
+): Pick<PR, "labels" | "author" | "reviewers"> {
+  return {
+    labels: detail.labels.nodes.map((label) => label.name),
+    author: detail.author.login,
+    reviewers: detail.reviews.nodes.map((review) => review.author.login),
+  };
+}
+
+export function combinePRData(listItem: PRListItem, detail: PRDetail): PR {
+  const prSummary = transformPRListItem(listItem);
+  const prDetail = transformPRDetail(detail);
+
+  return {
+    ...prSummary,
+    ...prDetail,
+  };
+}
