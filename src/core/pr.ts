8c54cedc086f4972de742456d789ed41dcaf5996
yuza <untmd.crmn@gmail.com>
2025-07-04 23:21:37 +0900

    feat: implement getUserCreatedPRListInPeriodByLabel function

--- src/core/pr.ts
+++ src/core/pr.ts
@@ -6,3 +6,15 @@ export async function getUserCreatedPRListInPeriod(options: ActivityQueryOptions
 
   return prList.filter((pr) => pr.author === options.username);
 }
+
+export async function getUserCreatedPRListInPeriodByLabel(
+  options: ActivityQueryOptions,
+): Promise<PR[]> {
+  const prList = await fetchAllPRListInPeriod(options);
+
+  return prList.filter(
+    (pr) =>
+      pr.author === options.username &&
+      pr.labels.some((label) => options.labelFilter?.includes(label)),
+  );
+}
