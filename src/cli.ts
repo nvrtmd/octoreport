9794e5064ea8bfc4869fe57c35ea4d1560f90896
yuza <untmd.crmn@gmail.com>
2025-07-06 12:46:23 +0900

    feat(cli): migrate CLI to ESM bundle with improved execution and compatibility

---
+++ src/cli.ts
@@ -0,0 +1,21 @@
+import { getUserCreatedPRListInPeriod } from './core/pr.js';
+
+const args = process.argv.slice(2);
+
+const username = args[0];
+const repository = args[1];
+const period = {
+  startDate: args[2],
+  endDate: args[3],
+};
+const targetBranch = args[4];
+
+const prList = await getUserCreatedPRListInPeriod({
+  githubToken: process.env.GITHUB_TOKEN ?? '',
+  username,
+  repository,
+  period,
+  targetBranch,
+});
+
+console.log(prList);
