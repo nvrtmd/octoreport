90f7a434e8ed8538561ff30c2c10f75de0193b18
yuza <untmd.crmn@gmail.com>
2025-07-06 14:59:06 +0900

    test(core): create test case to test getUserPRCountByLabelInPeriod

--- tests/core/pr.test.ts
+++ tests/core/pr.test.ts
@@ -1,7 +1,11 @@
 import { config } from 'dotenv';
 import { describe, expect, it } from 'vitest';
 
-import { getUserCreatedPRListInPeriod, getUserCreatedPRListInPeriodByLabel } from '../../src/core';
+import {
+  getUserCreatedPRListInPeriod,
+  getUserCreatedPRListInPeriodByLabel,
+  getUserPRCountByLabelInPeriod,
+} from '../../src/core';
 config();
 
 describe('getUserCreatedPRListInPeriod', () => {
@@ -70,5 +74,30 @@ describe('getUserCreatedPRListInPeriodByLabel', () => {
     expect(result[1].labels).toContain('regression 🐛');
     expect(result[2].labels).toContain('regression 🐛');
     expect(result).toMatchSnapshot();
+  });
+});
+
+describe('getUserPRCountByLabelInPeriod', () => {
+  it('counts pull requests created by the user within the specified period, grouped by label, and returns an object like {feat: 10, fix: 2, test: 10, ...}', async () => {
+    const result = await getUserPRCountByLabelInPeriod({
+      githubToken: process.env.GITHUB_TOKEN ?? '',
+      username: 'oliviertassinari',
+      repository: 'mui/material-ui',
+      period: { startDate: '2024-09-10', endDate: '2024-09-20' },
+      targetBranch: 'mui:v5.x',
+    });
+    expect(result).toEqual({
+      'scope: docs-infra': 3,
+      security: 1,
+      core: 3,
+      'component: slider': 1,
+      docs: 6,
+      'regression 🐛': 4,
+      'v5.x': 2,
+      'bug 🐛': 2,
+      examples: 1,
+      test: 1,
+      website: 2,
+    });
   });
 });
