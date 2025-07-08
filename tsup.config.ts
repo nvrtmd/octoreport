f085787a3bd48b9c437463ca107ca6863d5564c4
yuza <untmd.crmn@gmail.com>
2025-07-08 17:10:00 +0900

    refactore: remove banner field from tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,5 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/cli.ts'],
   clean: true,
-  banner: {
-    js: '#!/usr/bin/env node',
-  },
   external: ['dotenv'],
 });
