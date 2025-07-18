a178d3ad64eb69c600fdbb86076a7ad8dea87703
yuza <untmd.crmn@gmail.com>
2025-07-18 22:16:16 +0900

    refactor: remove CLI dependencies and restructure as core library

--- src/cli/index.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import { createTotalCommand } from './commands';
-import { createCommander } from './setup';
-
-const program = createCommander();
-
-createTotalCommand(program);
-
-program.parse();
