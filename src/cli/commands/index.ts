b1d46727f142604e4fb0e4cef25e4b667b772fff
yuza <untmd.crmn@gmail.com>
2025-07-18 22:23:44 +0900

    refactor: convert to core library by removing CLI dependencies (#17)

--- src/cli/commands/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './total';
-export * from './withCommonSetup';
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from './api';
+export * from './core';
+export * from './schemas';
+export * from './types';
+export * from './auth';
